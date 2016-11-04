cf4356c5e5e9df1c4b1a1a846a568f2eb7098905
dgarrotegordon <dgarrotegordon@gmail.com>
2016-11-04 17:57:02 +0100

    Ejercicio01

---
+++ Ejercicio01/src/Ejercicio1.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Ejercicio1 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner teclado=new Scanner(System.in);
+int A;
+int B;
+int resultado=0;
+System.out.println("Inserte valor de A: ");
+A = teclado.nextInt();
+System.out.println("Inserte valor de B: ");
+B = teclado.nextInt();
+
+while (A != B-1){
+	A++;
+	resultado = resultado + A;
+
+}
+System.out.println(resultado);
+
+
+		
+		
+}
+}
