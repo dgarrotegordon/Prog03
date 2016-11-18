0bdbc2b87e690a51cf60bcd5f190f83b815853ee
dgarrotegordon <dgarrotegordon@gmail.com>
2016-11-18 18:55:46 +0100

    Ejercicios 10 y 11

--- Ejercicio10/src/Ejercicio10.java
+++ Ejercicio10/src/Ejercicio10.java
@@ -4,5 +4,19 @@ public class Ejercicio10 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 Scanner teclado = new Scanner(System.in);
+int N, divisor; boolean esPrimo = false;
+N = teclado.nextInt();
+	while(!esPrimo){
+		N++;
+		esPrimo = true;
+		divisor = 2;
+		while(esPrimo && divisor<N){
+			esPrimo = N % divisor != 0;
+			divisor++;
+					}
+		if(esPrimo){
+			System.out.println("El numero primo mas cercano por exceso es: " + N);
+			}
+	}
 	}
 	}
--- Ejercicio11/src/Ejercicio11.java
+++ Ejercicio11/src/Ejercicio11.java
@@ -1,32 +1,55 @@
-import java.util.Scanner;
+import java.util.*;
 public class Ejercicio11 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 Scanner teclado = new Scanner(System.in);
-int N, numero=1, divisor=2, divisores, modulo;
-boolean esPrimo;
+int N, numero=1, divisor;
+boolean esPrimo=true;
 System.out.println("Introduzca un número: ");
 N = teclado.nextInt();
-	while(numero<=N){
-	divisores=0;	
-	divisor=2;
-		while(divisor <= numero){
-			modulo = numero%divisor;
-			divisor++;
-			if (modulo==0){
-				divisores++;}
-			if (divisores!=0){
-				esPrimo=false;
-			System.out.print(numero);
-			}
-			}
-numero++;}
 
+if (N <= 1) {
+	System.out.print("1");
+} else if (N <= 2) {
+	System.out.print("1 2");
+} else {
 	
+	System.out.print("1 2 3 ");
+	numero  = 4;
+	
+	while(numero<=N) {
 		
+//		while (numero==1 || numero==2 || numero==3){
+//			System.out.print(numero + " ");
+//			numero++;
+//		}
+		//	System.out.println(numero + " " + N);
+		if(numero%2!=0) {
+			divisor=3;
+			
+			while (esPrimo && divisor<numero){
+				if(numero%divisor!=0){
+					///esPrimo=true;						
+					divisor+=2;
+				}
+				else{
+					esPrimo=false;	
+				}
+			}
+			
+			if (esPrimo){
+				System.out.println(numero+ " ");
+			} else {
+				esPrimo = true; // lo ponemos a cierto, para comprobar el siguiente número
+			}
+		}
+		
+		numero++;
 	}
+}
+
+}
 	
-	}
-	
+}
 	
